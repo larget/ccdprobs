51c49f9c4f0e774af096130e86a4bdd3c1cdde30
Bret Larget <bret.larget@wisc.edu>
2018-09-24 12:58:38 -0500

    removed lots of output files

--- Bistro/Examples/CCDW/summary2.R
+++
@@ -1,9 +0,0 @@
-## just want to identify one odd point
-
-source("../../Scripts/readBistro.r")
-bistro = readBistro("test11-3")
-data = readDataSort("test11-3")
-head(bistro)
-bistro[bistro$w>0.6,] ## 576
-data[576,]
-summary(bistro)
--- Bistro/Examples/cats-dogs/cats-dogs.sh
+++
@@ -1,2 +0,0 @@
-../../Code/bistro/bistro -f ../../Data/cats-dogs.fasta -b 10000 >& cats-dogs.out
-
